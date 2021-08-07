3ed7b0b354a1384bf7362340aae8d635c674dcf0
choign3123 <choign3123@naver.com>
2021-08-08 01:58:29 +0900

    변수 이름 수정, 메뉴바 조금 수정
    -변수 이름 좀더 알아보기 쉽게 바꿨어요.
    -메뉴바는 창 바깥쪽 클릭하면 창 닫을 수 있게 만들어줬습니다. 이게 <div>안에 넣어서 만드는 걸 생각하는 게 어려웠네요.

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 "use strict"
 
 document.addEventListener("DOMContentLoaded", () => {
-    const menuWrap_xButton = document.querySelector("#navbar_x");
-    const menu_button = document.querySelector("#nav_menubar");
+    const menuWrap_xButton = document.querySelector("#menubar_x");
+    const menu_button = document.querySelector("#nav_menuBtn");
     const menuWrap = document.querySelector("#menuWrap");
+    const overlay = document.querySelector("#menubar_overlay");
 
     menu_button.addEventListener("click", (event) => {
         menuWrap.classList.toggle("on"); //menuWrap에 "on"이라는 클래스가 없으면 추가함. 있으면 없애고. ex) <span id="menuWrap" class="on">
+        overlay.classList.toggle("on");
         console.log("click button");
     })
     menuWrap_xButton.addEventListener("click", (event) => {
         menuWrap.classList.toggle("on");
+        overlay.classList.toggle("on");
         console.log("click button2");
+    })
+    overlay.addEventListener("click", (event) => {
+        if(event.target.contains(menuWrap)){
+            console.log("click button3");
+            // console.log(event.target);
+            menuWrap.classList.remove("on");
+            overlay.classList.remove("on");
+        }
     })
 })
