56c66887e26345bf8a9123f2cadbaf28bfd0fb64
choign3123 <choign3123@naver.com>
2021-09-09 15:51:55 +0900

    top 전체적으로 수정

--- top/top-dataProducter.js
+++ top/top-dataProducter.js
@@ -1,4 +1,4 @@
-const data = {
+export const data = {
     showNum: 8,
     clothNum: 16,
     cloth:[
@@ -42,32 +42,45 @@ const data = {
             price: "15,000",
             img: "../img/clothes1.jpg"
         },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes1.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes1.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes1.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes1.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes1.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes1.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes1.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes1.jpg"
+        },
     ]
 }
-
-const clothBundle = document.querySelector("#closet-dataArea");
-
-data.cloth.forEach((cloth) => {
-    const temp = document.createElement("div");
-    temp.className = "clothes";
-    temp.innerHTML = `
-        <a><img class = "clothesImg" src="${cloth.img}" alt="${cloth.name}"></a>
-        <p><a>${cloth.name}</a></p>
-        <p>${cloth.price}</p>
-    `;
-    clothBundle.appendChild(temp);
-})
-
-const showMore_Btn = document.querySelector(".showMore__btn");
-showMore_Btn.addEventListener("click", () => {
-    data.cloth.forEach((cloth) => {
-        const temp = document.createElement("div");
-        temp.className = "clothes";
-        temp.innerHTML = `
-            <a><img class = "clothesImg" src="${cloth.img}" alt="${cloth.name}"></a>
-            <p><a>${cloth.name}</a></p>
-            <p>${cloth.price}</p>
-        `;
-        clothBundle.appendChild(temp);
-    })
-})
