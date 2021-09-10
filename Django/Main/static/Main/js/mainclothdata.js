f06f13a33ba048f73f0e0c18d8c54245ad2f4e64
JinAru16 <wjdcksdnr11@naver.com>
2021-09-10 16:53:06 +0900

    Main, Top 문제
    Main, Top에 css 미적용문제

---
+++ Django/Main/static/Main/js/mainclothdata.js
@@ -0,0 +1,96 @@
+"use strict"
+
+const clothsData = {
+    "weeklyBest": [
+        {
+            "rank":1,
+            "name": "썸머 쿨 아리랑",
+            "price": "15,000",
+            "pagelink": "../product/product-detail.html",
+            "img": "../img/clothes1.jpg"
+        },
+        {
+            "rank":2,
+            "name": "베이직 니트",
+            "price": "24,000",
+            "pagelink": "../product/product-detail.html",
+            "img": "../img/clothes2.jpg"
+        },
+        {
+            "rank":3,
+            "name": "체크 카디건",
+            "price": "35,000",
+            "pagelink": "../product/product-detail.html",
+            "img": "../img/clothes3.jpg"
+        },
+        {
+            "rank":4,
+            "name": "포근한 카디건",
+            "price": "28,000",
+            "pagelink": "../product/product-detail.html",
+            "img": "../img/clothes4.jpg"
+        }
+    ],
+
+    "monthlyBest": [
+        {
+            "rank":1,
+            "name": "썸머 쿨 아리랑",
+            "price": "15,000",
+            "pagelink": "../product/product-detail.html",
+            "img": "../img/clothes3.jpg"
+        },
+        {
+            "rank":2,
+            "name": "베이직 니트",
+            "price": "24,000",
+            "pagelink": "../product/product-detail.html",
+            "img": "../img/clothes4.jpg"
+        },
+        {
+            "rank":3,
+            "name": "체크 카디건",
+            "price": "35,000",
+            "pagelink": "../product/product-detail.html",
+            "img": "../img/clothes2.jpg"
+        },
+        {
+            "rank":4,
+            "name": "포근한 카디건",
+            "price": "28,000",
+            "pagelink": "../product/product-detail.html",
+            "img": "../img/clothes1.jpg"
+        }
+    ]
+}
+
+const weekly = document.querySelector("#bestProduct__weekly");
+const weeklyBestData = clothsData.weeklyBest;
+for(let i=0; i<weeklyBestData.length; i++){
+    let unit = document.createElement("div");
+    unit.className = "bestProduct__cloth";
+    unit.innerHTML = `
+        <a href="${weeklyBestData[i].pagelink}">
+            <img class = "clothesImg" src="${weeklyBestData[i].img}" alt="${weeklyBestData[i].name}" title="${weeklyBestData[i].name}">
+            <div>${weeklyBestData[i].name}</div>
+            <div>${weeklyBestData[i].price}</div>
+        </a>
+    `;
+    weekly.appendChild(unit);
+}
+
+const monthly = document.querySelector("#bestProduct__monthly");
+const monthlyBestData = clothsData.monthlyBest;
+
+for(let i=0; i<monthlyBestData.length; i++){
+    let unit = document.createElement("div");
+    unit.className = "bestProduct__cloth";
+    unit.innerHTML = `
+        <a href="${monthlyBestData[i].pagelink}">
+            <img class = "clothesImg" src="${monthlyBestData[i].img}" alt="${monthlyBestData[i].name}" title="${monthlyBestData[i].name}">
+            <div>${monthlyBestData[i].name}</div>
+            <div>${monthlyBestData[i].price}</div>
+        </a>
+    `;
+    monthly.appendChild(unit);
+}
---
+++ Django/Top/static/Top/js/topdata.js
@@ -0,0 +1,73 @@
+const data = {
+    showNum: 8,
+    clothNum: 16,
+    cloth:[
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes4.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes3.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes1.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes2.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes4.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes2.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes3.jpg"
+        },
+        {
+            name: "썸머 쿨 아리랑",
+            price: "15,000",
+            img: "../img/clothes1.jpg"
+        },
+    ]
+}
+
+const clothBundle = document.querySelector("#closet-dataArea");
+
+data.cloth.forEach((cloth) => {
+    const temp = document.createElement("div");
+    temp.className = "clothes";
+    temp.innerHTML = `
+        <a><img class = "clothesImg" src="${cloth.img}" alt="${cloth.name}"></a>
+        <p><a>${cloth.name}</a></p>
+        <p>${cloth.price}</p>
+    `;
+    clothBundle.appendChild(temp);
+})
+
+const showMore_Btn = document.querySelector(".showMore__btn");
+showMore_Btn.addEventListener("click", () => {
+    data.cloth.forEach((cloth) => {
+        const temp = document.createElement("div");
+        temp.className = "clothes";
+        temp.innerHTML = `
+            <a><img class = "clothesImg" src="${cloth.img}" alt="${cloth.name}"></a>
+            <p><a>${cloth.name}</a></p>
+            <p>${cloth.price}</p>
+        `;
+        clothBundle.appendChild(temp);
+    })
+})
